19b658df1f2247cb9a0b63c9daed16d7f996c884
c0d-a <c0d-a@users.noreply.github.com>
2020-09-30 17:08:11 -0400

    fixed grammar and deleted unnecessary white space

--- ServerSide/server.py
+++ ServerSide/server.py
@@ -1,22 +1,22 @@
-import socket                        #module to establish connection
+import socket  # module to establish connection
 
-# Initializes the socket obj, hostname and port and binds it to the server
+# initializes the socket obj, hostname and port and binds it to the server
 socketVar = socket.socket()
 hostName = socket.gethostname()
 port = 8090
 socketVar.bind((hostName, port))
-socketVar.listen(1) # wait for 1 incoming connection
+socketVar.listen(1)  # wait for 1 incoming connection
 
 print(hostName)
 
-# Loops to accept the incoming connection and file being sent from the client
+# loops to accept the incoming connection and file being sent from the client
 while True:
     print("Waiting for connection...")
     connection, address = socketVar.accept()
 
     print(address, "Has connected to the server")
 
-    # Receives the fileName and packets from the client and decodes it
+    # receives the fileName and packets from the client and decodes it
     fileName = connection.recv(1024)
     print(fileName)
     fileName = fileName.decode()
@@ -30,7 +30,7 @@ while True:
     # loops to keep receiving packets and prints the packets being received from the client
     for x in range(1, numOfPackets + 1):
         numOfPacketsRecv_String = f"Receiving packet #{x} from client..."
-        print (numOfPacketsRecv_String)
+        print(numOfPacketsRecv_String)
         data = connection.recv(1024)
         file.write(data)
     connection.close()
--- client.py
+++ client.py
@@ -1,12 +1,12 @@
-import socket                                                          # module to establish connection
-import tkinter as tk                                                   # module to create GUI
-import time                                                            # module for time funcs such as .sleep()
+import socket                    # module to establish connection
+import tkinter as tk             # module to create GUI
+import time                      # module for time funcs such as .sleep()
 
 def transmitFile(hostAddress, fileName):
-    #function to transmit the file. Contains the code copy/pasted from phase1
-    #takes as input the host address to send the file to and the name for the file upon arrival
+    # function to transmit the file. Contains the code copy/pasted from phase1
+    # takes as input the host address to send the file to and the name for the file upon arrival
 
-    #intitialization of the object and connects it to the port and host address
+    # initialization of the object and connects it to the port and host address
     socketVar = socket.socket()
     port = 8090
     socketVar.connect((hostAddress, port))
@@ -23,7 +23,7 @@ def transmitFile(hostAddress, fileName):
     print(fileName)
     print(numOfPackets)
 
-    # Encodes the fileName and numOfPackets and sends it to the server
+    # encodes the fileName and numOfPackets and sends it to the server
     encodedFileName = fileName.encode()
     socketVar.send(encodedFileName)
     time.sleep(1) # delays by 1 second
@@ -39,13 +39,13 @@ def transmitFile(hostAddress, fileName):
         socketVar.send(data)
     fileToSend.close()
 
-    # Displays that the data has been sent successfully
-    print ("\nData has been sent successfully!")
+    # displays that the data has been sent successfully
+    print("\nData has been sent successfully!")
 
     return
 
 def sendFile(event):
-    #event to send the file once the user has clicked the "Send file" button
+    # event to send the file once the user has clicked the "Send file" button
     hostAddress = ent_destination.get()
     fileName = ent_fileName.get()
     print(fileName)
@@ -53,23 +53,23 @@ def sendFile(event):
     transmitFile(hostAddress, fileName)
     return
 
-#Get the name of this machine to use as a default address
+# get the name of this machine to use as a default address
 defaultServerName = socket.gethostname()
 window = tk.Tk()
-#introductory message
+# introductory message
 lbl_introduction = tk.Label(text = "Networking Design Project Phase 2. \n"
                              "EECE.4830 201. Professor Vokkarane. \n"
                              "By: Julie Dawley, Ricardo Candanedo, and Mohammad Musawer \n \n"
                              "Destination Computer: Defaults to this machine. \n"
                              "Change to the address in serverClient if running client and server on seperate machines")
 lbl_introduction.pack()
-#Get the destination name from the user, default to defaultServerName
+# get the destination name from the user, default to defaultServerName
 ent_destination = tk.Entry()
 ent_destination.pack()
 ent_destination.insert(0, defaultServerName)
-#hostAddress = ent_destination.get()
+# hostAddress = ent_destination.get()
 
-#get the file name from the user. Default to receivedFile.jpg
+# get the file name from the user. Default to receivedFile.jpg
 lbl_getFileName = tk.Label(text="\n Enter the name the file should have at the destination")
 ent_fileName = tk.Entry()
 lbl_getFileName.pack()
@@ -81,4 +81,4 @@ btn_confirmEntry = tk.Button(text="Transmit File", height=2, width=10)
 btn_confirmEntry.pack()
 btn_confirmEntry.bind('<Button-1>', sendFile)
 
-window.mainloop() # Keeps window open until event is called or the user exits the GUI
+window.mainloop()  # keeps window open until event is called or the user exits the GUI
